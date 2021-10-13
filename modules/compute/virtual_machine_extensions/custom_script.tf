5886faf90e6a8857e931015fa8d6fba47ba973bf
dimitrifc <83534599+dimitrifc@users.noreply.github.com>
2021-10-13 15:26:09 +0200

    add managed identity

--- modules/compute/virtual_machine_extensions/custom_script.tf
+++ modules/compute/virtual_machine_extensions/custom_script.tf
@@ -16,7 +16,31 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
   protected_settings = jsonencode(
     {
       "commandToExecute" : try(var.extension.commandtoexecute, ""),
-      "managedIdentity" : try(var.extension.managedidentity, "")
+      "managedIdentity" : {
+        objectid : try(local.managed_identities, "")     
+      }      
     }
   )
+}
+
+#
+# Managed identities from remote state
+#
+
+locals {
+  managed_local_identities = flatten([
+    for managed_identity_key in try(var.extension.managed_identity_keys, []) : [
+      var.managed_identities[var.client_config.landingzone_key][managed_identity_key].id
+    ]
+  ])
+
+  managed_remote_identities = flatten([
+    for lz_key, value in try(var.extension.identity.remote, []) : [
+      for managed_identity_key in value.managed_identity_keys : [
+        var.managed_identities[lz_key][managed_identity_key].id
+      ]
+    ]
+  ])
+
+  managed_identities = concat(local.managed_local_identities, local.managed_remote_identities)
 }
