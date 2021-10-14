95172becc0e8fa8f2fe20b9694a597c51916cb0d
dimitrifc <83534599+dimitrifc@users.noreply.github.com>
2021-10-14 09:43:15 +0200

    principal_id instead of mi id

--- modules/compute/virtual_machine_extensions/custom_script.tf
+++ modules/compute/virtual_machine_extensions/custom_script.tf
@@ -30,14 +30,14 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
 locals {
   managed_local_identities = flatten([
     for managed_identity_key in try(var.extension.managed_identity_keys, []) : [
-      var.managed_identities[var.client_config.landingzone_key][managed_identity_key].id
+      var.managed_identities[var.client_config.landingzone_key][managed_identity_key].principal_id
     ]
   ])
 
   managed_remote_identities = flatten([
     for lz_key, value in try(var.extension.identity.remote, []) : [
       for managed_identity_key in value.managed_identity_keys : [
-        var.managed_identities[lz_key][managed_identity_key].id
+        var.managed_identities[lz_key][managed_identity_key].principal_id
       ]
     ]
   ])
