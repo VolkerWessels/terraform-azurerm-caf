1ac5f47fb3af927f1f4367a09d229d2a9785c964
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-04 15:18:37 +0100

    Uncomment monitor_autoscale_setting.tf and change var name

--- monitor_autoscale_setting.tf
+++ monitor_autoscale_setting.tf
@@ -1,8 +1,8 @@
-# module "monitor_autoscale_setting" {
-#   source              = "./modules/monitor_autoscale_setting"
-#   for_each            = local.shared_services.monitor_autoscale_setting
-#   global_settings     = local.global_settings
-#   settings            = each.value
-#   resource_group_name = local.resource_groups[each.value.resource_group_key].name
-#   location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-# }
+module "monitor_autoscale_setting" {
+  source              = "./modules/monitor_autoscale_setting"
+  for_each            = local.shared_services.monitor_autoscale_setting
+  global_settings     = local.global_settings
+  settings            = each.value
+  resource_group_name = local.resource_groups[each.value.resource_group_key].name
+  location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+}
