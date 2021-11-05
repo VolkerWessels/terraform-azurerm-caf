61eea7f188c0deea969365958b1ca7e40c6101f8
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-05 15:38:58 +0100

    Use the id output of module.virtual_machine_scale_sets

--- monitor_autoscale_settings.tf
+++ monitor_autoscale_settings.tf
@@ -6,6 +6,5 @@ module "monitor_autoscale_settings" {
   settings            = each.value
   resource_group_name = local.resource_groups[each.value.resource_group_key].name
   location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  target_resource_id  = module.virtual_machine_scale_sets.azurerm_virtual_machine_scale_set.vmss[try(each.value.virtual_machine_scale_sets.key)].id
-  # target_resource_id  = azurerm_virtual_machine_scale_set.vmss[try(each.value.virtual_machine_scale_sets.key)].id
+  target_resource_id  = module.virtual_machine_scale_sets.id
 }
