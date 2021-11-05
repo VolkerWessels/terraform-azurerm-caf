fdc3fad86a6f0e9f5fa5d1098a8a6e770c677c0d
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-05 14:26:04 +0100

    Add target_resource_id

--- monitor_autoscale_settings.tf
+++ monitor_autoscale_settings.tf
@@ -5,4 +5,5 @@ module "monitor_autoscale_settings" {
   settings            = each.value
   resource_group_name = local.resource_groups[each.value.resource_group_key].name
   location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  target_resource_id  = azurerm_virtual_machine_scale_set.vmss[each.value.virtual_machine_scale_sets.key].id
 }
