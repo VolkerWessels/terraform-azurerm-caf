83308e74e3c2730ab29c2849245bbb18bb866d93
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-05 13:07:08 +0100

    Add extra variable docs

--- modules/monitoring/monitor_autoscale_settings/variables.tf
+++ modules/monitoring/monitor_autoscale_settings/variables.tf
@@ -2,4 +2,8 @@ variable "settings" {}
 variable "global_settings" {}
 variable "resource_group_name" {}
 variable "location" {}
-variable "monitor_autoscale_settings" {}
+variable "monitor_autoscale_settings" {
+  type = map
+  default = {}
+  description = "The map from the monitor_autoscale_settings module configuration"
+}
