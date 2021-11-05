91a3cfd0d3d4f72e47b065a3d4daef198178c272
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-05 15:54:03 +0100

    Add profile to top level and refactor

--- modules/monitoring/monitor_autoscale_settings/monitor_autoscale_settings.tf
+++ modules/monitoring/monitor_autoscale_settings/monitor_autoscale_settings.tf
@@ -15,75 +15,75 @@ resource "azurerm_monitor_autoscale_setting" "this" {
   location            = var.location
   target_resource_id  = var.target_resource_id
 
-  dynamic "profile_name" {
-    for_each = try(var.monitor_autoscale_settings, {})
-    content {
-      profile {
+  profile {
+    dynamic "profile_name" {
+      for_each = try(var.monitor_autoscale_settings, {})
+      content {
         name = profile.value.name
       }
     }
+  }
 
-    dynamic "capacity" {
-      for_each = try(var.monitor_autoscale_settings.profile, {})
+  dynamic "capacity" {
+    for_each = try(var.monitor_autoscale_settings.profile, {})
+    content {
+      capacity {
+        default = capacity.value.default
+        minimum = capacity.value.minimum
+        maximum = capacity.value.maximum
+      }
+    }
+  }
+
+  rule {
+    dynamic "metric_trigger" {
+      for_each = try(var.monitor_autoscale_settings.rule, {})
       content {
-        capacity {
-          default = capacity.value.default
-          minimum = capacity.value.minimum
-          maximum = capacity.value.maximum
+        metric_trigger {
+          metric_name        = metric_trigger.value.metric_name
+          metric_resource_id = metric_trigger.value.metric_resource_id
+          time_grain         = metric_trigger.value.time_grain
+          statistic          = metric_trigger.value.statistic
+          time_window        = metric_trigger.value.time_window
+          time_aggregation   = metric_trigger.value.time_aggregation
+          operator           = metric_trigger.value.operator
+          threshold          = metric_trigger.value.threshold
         }
       }
     }
-
-    rule {
-      dynamic "metric_trigger" {
-        for_each = try(var.monitor_autoscale_settings.rule, {})
-        content {
-          metric_trigger {
-            metric_name        = metric_trigger.value.metric_name
-            metric_resource_id = metric_trigger.value.metric_resource_id
-            time_grain         = metric_trigger.value.time_grain
-            statistic          = metric_trigger.value.statistic
-            time_window        = metric_trigger.value.time_window
-            time_aggregation   = metric_trigger.value.time_aggregation
-            operator           = metric_trigger.value.operator
-            threshold          = metric_trigger.value.threshold
-          }
-        }
-      }
-      dynamic "scale_action" {
-        for_each = try(var.monitor_autoscale_settings.rule, {})
-        content {
-          scale_action {
-            direction = scale_action.value.direction
-            type      = scale_action.value.type
-            value     = scale_action.value.value
-            cooldown  = scale_action.value.cooldown
-          }
-        }
-      }
-      dynamic "recurrence" {
-        for_each = try(var.monitor_autoscale_settings.rule, {})
-        content {
-          recurrence {
-            frequency = recurrence.value.frequency
-            timezone  = recurrence.value.timezone
-            days      = recurrence.value.days
-            hours     = recurrence.value.hours
-            minutes   = recurrence.value.minutes
-          }
+    dynamic "scale_action" {
+      for_each = try(var.monitor_autoscale_settings.rule, {})
+      content {
+        scale_action {
+          direction = scale_action.value.direction
+          type      = scale_action.value.type
+          value     = scale_action.value.value
+          cooldown  = scale_action.value.cooldown
         }
       }
     }
+    dynamic "recurrence" {
+      for_each = try(var.monitor_autoscale_settings.rule, {})
+      content {
+        recurrence {
+          frequency = recurrence.value.frequency
+          timezone  = recurrence.value.timezone
+          days      = recurrence.value.days
+          hours     = recurrence.value.hours
+          minutes   = recurrence.value.minutes
+        }
+      }
+    }
+  }
 
-    notification {
-      dynamic "email" {
-        for_each = try(var.monitor_autoscale_settings.notification, {})
-        content {
-          email {
-            send_to_subscription_administrator    = email.value.send_to_subscription_administrator
-            send_to_subscription_co_administrator = email.value.send_to_subscription_co_administrator
-            custom_emails                         = email.value.custom_emails
-          }
+  notification {
+    dynamic "email" {
+      for_each = try(var.monitor_autoscale_settings.notification, {})
+      content {
+        email {
+          send_to_subscription_administrator    = email.value.send_to_subscription_administrator
+          send_to_subscription_co_administrator = email.value.send_to_subscription_co_administrator
+          custom_emails                         = email.value.custom_emails
         }
       }
     }
