76c34bf017b46541e71dbfca682fd05c78fdad73
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-09 14:02:49 +0100

    Change variable to local

--- monitor_autoscale_settings.tf
+++ monitor_autoscale_settings.tf
@@ -5,5 +5,5 @@ module "monitor_autoscale_settings" {
   global_settings            = local.global_settings
   resource_group_name        = local.resource_groups[each.value.resource_group_key].name
   location                   = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  target_resource_id         = module.virtual_machine_scale_sets.id[var.virtual_machine_scale_sets.keys]
+  target_resource_id         = module.virtual_machine_scale_sets.id[local.compute.virtual_machine_scale_sets.keys]
 }
