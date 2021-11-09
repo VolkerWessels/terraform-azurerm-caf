a4e474335c81b4a537e2602c13d94010a7815f4f
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-09 15:18:54 +0100

    Add type checking again

--- modules/monitoring/monitor_autoscale_settings/variables.tf
+++ modules/monitoring/monitor_autoscale_settings/variables.tf
@@ -3,5 +3,7 @@ variable "resource_group_name" {}
 variable "location" {}
 variable "target_resource_id" {}
 variable "monitor_autoscale_settings" {
+  type = map
+  default = {}
   description = "The map from the monitor_autoscale_settings module configuration"
 }
