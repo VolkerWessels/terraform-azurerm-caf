5dfa2a2aaaaed8445ef36b7aff4babf98e9be873
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-09 15:07:26 +0100

    Remove 'default = {}'

--- modules/monitoring/monitor_autoscale_settings/variables.tf
+++ modules/monitoring/monitor_autoscale_settings/variables.tf
@@ -3,6 +3,5 @@ variable "resource_group_name" {}
 variable "location" {}
 variable "target_resource_id" {}
 variable "monitor_autoscale_settings" {
-  default = {}
   description = "The map from the monitor_autoscale_settings module configuration"
 }
