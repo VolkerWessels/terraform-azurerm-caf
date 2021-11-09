e65e45302eb38453e9547630b363eb2ca7eb88d1
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-09 13:14:01 +0100

    Pass the monitor_autoscale_settings map

--- monitor_autoscale_settings.tf
+++ monitor_autoscale_settings.tf
@@ -1,10 +1,9 @@
 module "monitor_autoscale_settings" {
-  depends_on          = [module.virtual_machine_scale_sets]
-  source              = "./modules/monitoring/monitor_autoscale_settings"
-  for_each            = local.shared_services.monitor_autoscale_settings
-  global_settings     = local.global_settings
-  settings            = each.value
-  resource_group_name = local.resource_groups[each.value.resource_group_key].name
-  location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  target_resource_id  = module.virtual_machine_scale_sets.id
+  depends_on                 = [module.virtual_machine_scale_sets]
+  source                     = "./modules/monitoring/monitor_autoscale_settings"
+  monitor_autoscale_settings = local.shared_services.monitor_autoscale_settings
+  global_settings            = local.global_settings
+  resource_group_name        = local.resource_groups[each.value.resource_group_key].name
+  location                   = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  target_resource_id         = module.virtual_machine_scale_sets.id[var.virtual_machine_scale_sets.keys]
 }
