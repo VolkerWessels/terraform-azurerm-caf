cefdb0f8cce67c7985b1e10ce4287ecd841c3cf0
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-10 15:48:38 +0100

    Remove redundant spaces and change resource_group_name

--- monitor_autoscale_settings.tf
+++ monitor_autoscale_settings.tf
@@ -1,10 +1,10 @@
 module "monitor_autoscale_settings" {
-  depends_on                 = [module.virtual_machine_scale_sets]
-  source                     = "./modules/monitoring/monitor_autoscale_settings"
-  for_each                   = local.shared_services.monitor_autoscale_settings
-  settings                   = each.value
-  global_settings            = local.global_settings
-  resource_group_name        = local.resource_groups[each.value.resource_group_key].name
-  location                   = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  target_resource_id         = module.virtual_machine_scale_sets.id[local.compute.virtual_machine_scale_sets.keys]
+  depends_on          = [module.virtual_machine_scale_sets]
+  source              = "./modules/monitoring/monitor_autoscale_settings"
+  for_each            = local.shared_services.monitor_autoscale_settings
+  settings            = each.value
+  global_settings     = local.global_settings
+  resource_group_name = local.combined_objects_resource_groups[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.resource_group_key].name
+  location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  target_resource_id  = module.virtual_machine_scale_sets.id[local.compute.virtual_machine_scale_sets.keys]
 }
