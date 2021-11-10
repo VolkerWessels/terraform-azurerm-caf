ad088b432197bd42d06f5f3d09eb1de300e57609
Dimitri <>
2021-11-10 15:41:54 +0100

    add vmss exts vars

--- modules/compute/virtual_machine_scale_set_extensions/variables.tf
+++ modules/compute/virtual_machine_scale_set_extensions/variables.tf
@@ -1,5 +1,13 @@
+variable "virtual_machine_scale_set_id" {}
 variable "extension" {}
 variable "extension_name" {}
 variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
+variable "managed_identities" {
+  default = {}
+}
+variable "storage_accounts" {
+  default = {}
+}
+
