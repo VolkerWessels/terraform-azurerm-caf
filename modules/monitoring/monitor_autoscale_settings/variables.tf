e0c6de093b3f64acba39dd9766a47ad7caf6e1dd
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-10 11:20:27 +0100

    Remove monitor_autoscale_settings variable

--- modules/monitoring/monitor_autoscale_settings/variables.tf
+++ modules/monitoring/monitor_autoscale_settings/variables.tf
@@ -2,10 +2,6 @@ variable "global_settings" {}
 variable "resource_group_name" {}
 variable "location" {}
 variable "target_resource_id" {}
-variable "monitor_autoscale_settings" {
-  default = {}
-  description = "The map from the monitor_autoscale_settings module configuration"
-}
 variable "settings" {
   default = {}
   description = "Configuration object for the monitor autoscale setting resource"
