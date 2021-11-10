189b37e2c52436e54ff208db16afe11e0c4af6dd
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-10 11:15:08 +0100

    Add default = {}

--- modules/monitoring/monitor_autoscale_settings/variables.tf
+++ modules/monitoring/monitor_autoscale_settings/variables.tf
@@ -3,8 +3,10 @@ variable "resource_group_name" {}
 variable "location" {}
 variable "target_resource_id" {}
 variable "monitor_autoscale_settings" {
+  default = {}
   description = "The map from the monitor_autoscale_settings module configuration"
 }
 variable "settings" {
+  default = {}
   description = "Configuration object for the monitor autoscale setting resource"
 }
