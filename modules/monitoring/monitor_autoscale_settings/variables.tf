506d546d691646ddd291387c05471ac5006e80e8
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-11 15:32:17 +0100

    Add virtual_machine_scale_sets variable

--- modules/monitoring/monitor_autoscale_settings/variables.tf
+++ modules/monitoring/monitor_autoscale_settings/variables.tf
@@ -2,6 +2,7 @@ variable "global_settings" {}
 variable "resource_group_name" {}
 variable "location" {}
 variable "target_resource_id" {}
+variable "virtual_machine_scale_sets" {}
 variable "settings" {
   default = {}
   description = "Configuration object for the monitor autoscale setting resource"
