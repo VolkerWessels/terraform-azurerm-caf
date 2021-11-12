b12e74873f2074e7dbed11e69cdf8dad9e7ff291
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-12 11:37:06 +0100

    Remove local.client_config.landingzone_key

--- monitor_autoscale_settings.tf
+++ monitor_autoscale_settings.tf
@@ -6,5 +6,5 @@ module "monitor_autoscale_settings" {
   global_settings     = local.global_settings
   resource_group_name = local.resource_groups[each.value.resource_group_key].name
   location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  target_resource_id  = local.compute.virtual_machine_scale_sets[local.client_config.landingzone_key][each.value.vmss_key].id
+  target_resource_id  = local.compute.virtual_machine_scale_sets[each.value.vmss_key].id
 }
