ae1f4a73279c6fb10c2acc968827c26759c2e158
hyperfocus1337 <hyperfocus@hotmail.com>
2021-11-19 14:24:56 +0100

    Add dynamic block for profile

--- modules/monitoring/monitor_autoscale_settings/monitor_autoscale_settings.tf
+++ modules/monitoring/monitor_autoscale_settings/monitor_autoscale_settings.tf
@@ -15,63 +15,67 @@ resource "azurerm_monitor_autoscale_setting" "this" {
   location            = var.location
   target_resource_id  = var.target_resource_id
 
-  profile {
-    name = var.settings.name
+  dynamic "profile" {
+    for_each = var.settings
+    content {
+      name = settings.name
 
-    capacity {
-      default = var.settings.capacity.default
-      minimum = var.settings.capacity.minimum
-      maximum = var.settings.capacity.maximum
-    }
+      capacity {
+        default = settings.capacity.default
+        minimum = settings.capacity.minimum
+        maximum = settings.capacity.maximum
+      }
 
-    dynamic "rule" {
-      for_each = var.settings.rules
-      content {
-        metric_trigger {
-          metric_name        = rule.value.metric_trigger.metric_name
-          metric_resource_id = try(rule.value.metric_trigger.metric_resource_id, var.target_resource_id)
-          time_grain         = rule.value.metric_trigger.time_grain
-          statistic          = rule.value.metric_trigger.statistic
-          time_window        = rule.value.metric_trigger.time_window
-          time_aggregation   = rule.value.metric_trigger.time_aggregation
-          operator           = rule.value.metric_trigger.operator
-          threshold          = rule.value.metric_trigger.threshold
+      dynamic "rule" {
+        for_each = var.settings.rules
+        content {
+          metric_trigger {
+            metric_name        = rule.value.metric_trigger.metric_name
+            metric_resource_id = try(rule.value.metric_trigger.metric_resource_id, var.target_resource_id)
+            time_grain         = rule.value.metric_trigger.time_grain
+            statistic          = rule.value.metric_trigger.statistic
+            time_window        = rule.value.metric_trigger.time_window
+            time_aggregation   = rule.value.metric_trigger.time_aggregation
+            operator           = rule.value.metric_trigger.operator
+            threshold          = rule.value.metric_trigger.threshold
+          }
+          scale_action {
+            direction = rule.value.scale_action.direction
+            type      = rule.value.scale_action.type
+            value     = rule.value.scale_action.value
+            cooldown  = rule.value.scale_action.cooldown
+          }
         }
-        scale_action {
-          direction = rule.value.scale_action.direction
-          type      = rule.value.scale_action.type
-          value     = rule.value.scale_action.value
-          cooldown  = rule.value.scale_action.cooldown
+      }
+
+      dynamic "recurrence" {
+        for_each = try(var.settings.recurrence, {})
+        content {
+          timezone = recurrence.timezone
+          days     = recurrence.days
+          hours    = recurrence.hours
+          minutes  = recurrence.minutes
         }
       }
+
+      dynamic "fixed_date" {
+        for_each = try(var.settings.fixed_date, {})
+        content {
+          timezone = try(fixed_date.timezone, "")
+          start    = fixed_date.start
+          end      = fixed_date.end
+        }
+      }
+
     }
 
-    dynamic "recurrence" {
-      for_each = try(var.settings.recurrences, {})
-      content {
-        timezone = recurrence.timezone
-        days     = recurrence.days
-        hours    = recurrence.hours
-        minutes  = recurrence.minutes
+    notification {
+      email {
+        send_to_subscription_administrator    = settings.notification.email.send_to_subscription_administrator
+        send_to_subscription_co_administrator = settings.notification.email.send_to_subscription_co_administrator
+        custom_emails                         = settings.notification.email.custom_emails
       }
     }
 
-    dynamic "fixed_date" {
-      for_each = try(var.settings.fixed_dates, {})
-      content {
-        timezone = try(fixed_date.timezone, "")
-        start    = fixed_date.start
-        end      = fixed_date.end
-      }
-    }
-
-  }
-
-  notification {
-    email {
-      send_to_subscription_administrator    = var.settings.notification.email.send_to_subscription_administrator
-      send_to_subscription_co_administrator = var.settings.notification.email.send_to_subscription_co_administrator
-      custom_emails                         = var.settings.notification.email.custom_emails
-    }
   }
 }
