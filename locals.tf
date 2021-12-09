7d068ba921bdeb1f3d669b4a5e509cbcb98c5832
dimitrifc <83534599+dimitrifc@users.noreply.github.com>
2021-12-09 14:52:27 +0100

    add packer_build module and examples

--- locals.tf
+++ locals.tf
@@ -305,6 +305,7 @@ locals {
     monitor_activity_log_alert = try(var.shared_services.monitor_activity_log_alert, {})
     packer_managed_identity    = try(var.shared_services.packer_managed_identity, {})
     packer_service_principal   = try(var.shared_services.packer_service_principal, {})
+    packer_build               = try(var.shared_services.packer_build, {})
     recovery_vaults            = try(var.shared_services.recovery_vaults, {})
     shared_image_galleries     = try(var.shared_services.shared_image_galleries, {})
   }
