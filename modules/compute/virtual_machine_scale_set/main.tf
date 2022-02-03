71d48bde79e4ba0ed6a23dff25888f36396a9ce9
Dimitri <>
2022-02-03 09:01:50 +0100

    move debug

--- modules/compute/virtual_machine_scale_set/main.tf
+++ modules/compute/virtual_machine_scale_set/main.tf
@@ -12,11 +12,11 @@ resource "null_resource" "debug" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-  command = "echo $VARIABLE1 >> image_definitions_debug.json; echo $VARIABLE2 >> image_definitions_debug.json; cat image_definitions_debug.json"
+    command = "echo $VARIABLE1 >> image_definitions_debug.json; echo $VARIABLE2 >> image_definitions_debug.json; cat image_definitions_debug.json"
     environment = {
       VARIABLE1 = jsonencode(var.application_gateways)
-      VARIABLE2 = jsonencode(var.boot_diagnostics_storage_account)
-    } 
+      VARIABLE2 = jsonencode(var.load_balancers)
+    }
   }
 }
 
