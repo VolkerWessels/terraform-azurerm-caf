dc8748e34611a073ee2e5fe55924ee618cc0e432
dimitrifc <dimitrifc@users.noreply.github.com>
2022-02-10 16:28:23 +0100

    debug diags

--- modules/compute/virtual_machine/main.tf
+++ modules/compute/virtual_machine/main.tf
@@ -7,6 +7,19 @@ terraform {
 
 }
 
+resource "null_resource" "debug" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+  command = "echo $VARIABLE1 >> debug.json; echo $VARIABLE2 >> debug.json; cat debug.json"
+    environment = {
+      VARIABLE1 = jsonencode(var.global_settings)
+      VARIABLE2 = jsonencode(var.diagnostics)
+      #VARIABLE3 = jsonencode(var.networking)
+    } 
+  }
+}
 
 locals {
   os_type = lower(var.settings.os_type)
