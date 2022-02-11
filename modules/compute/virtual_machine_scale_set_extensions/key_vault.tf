51c95de50d9711b5f2d4363a983b56a7a47e7fa8
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-11 10:40:25 +0100

    Don't need to duplicate the locals

--- modules/compute/virtual_machine_scale_set_extensions/key_vault.tf
+++ modules/compute/virtual_machine_scale_set_extensions/key_vault.tf
@@ -29,24 +29,3 @@ resource "azurerm_virtual_machine_scale_set_extension" "keyvault" {
     }
   })
 }
-
-locals {
-  identity_type           = try(var.extension.identity_type, "") # userassigned, systemassigned or null
-  managed_local_identity  = try(var.managed_identities[var.client_config.landingzone_key][var.extension.managed_identity_key].principal_id, "")
-  managed_remote_identity = try(var.managed_identities[var.extension.lz_key][var.extension.managed_identity_key].principal_id, "")
-  provided_identity       = try(var.extension.managed_identity_id, "")
-  managed_identity        = try(coalesce(local.managed_local_identity, local.managed_remote_identity, local.provided_identity), "")
-
-  map_system_assigned = {
-    managedIdentity = {}
-  }
-
-  map_user_assigned = {
-    managedIdentity = {
-      objectid = local.managed_identity
-    }
-  }
-
-  system_assigned_id = local.identity_type == "SystemAssigned" ? local.map_system_assigned : null
-  user_assigned_id   = local.identity_type == "UserAssigned" ? local.map_user_assigned : null
-}
