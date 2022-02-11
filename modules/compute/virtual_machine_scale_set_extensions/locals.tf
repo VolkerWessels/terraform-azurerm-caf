4fa33b1050c3fd1b2cfc7e835055e6916ccad28d
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-11 15:19:06 +0100

    Move back values specific to custom script extension

--- modules/compute/virtual_machine_scale_set_extensions/locals.tf
+++ modules/compute/virtual_machine_scale_set_extensions/locals.tf
@@ -4,23 +4,4 @@ locals {
   managed_remote_identity = try(var.managed_identities[var.extension.lz_key][var.extension.managed_identity_key].principal_id, "")
   provided_identity       = try(var.extension.managed_identity_id, "")
   managed_identity        = try(coalesce(local.managed_local_identity, local.managed_remote_identity, local.provided_identity), "")
-
-  map_system_assigned = {
-    managedIdentity = {}
-  }
-
-  map_user_assigned = {
-    managedIdentity = {
-      objectid = local.managed_identity
-    }
-  }
-
-  map_command = {
-    commandToExecute = try(var.extension.commandtoexecute, "")
-  }
-
-  system_assigned_id = local.identity_type == "SystemAssigned" ? local.map_system_assigned : null
-  user_assigned_id   = local.identity_type == "UserAssigned" ? local.map_user_assigned : null
-
-  protected_settings = merge(local.map_command, local.system_assigned_id, local.user_assigned_id)
 }
