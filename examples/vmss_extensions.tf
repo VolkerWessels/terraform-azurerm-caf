7aec0535387c4034e19da61919dcc9c7b2149cff
dimitrifc <dimitrifc@users.noreply.github.com>
2022-02-15 15:57:55 +0100

    add keyvault extension to examples

--- examples/vmss_extensions.tf
+++ examples/vmss_extensions.tf
@@ -32,3 +32,23 @@ module "vmss_extension_custom_scriptextension" {
   storage_accounts                  = tomap({ (var.landingzone.key) = module.example.storage_accounts })
   virtual_machine_scale_set_os_type = module.example.virtual_machine_scale_sets[each.key].os_type
 }
+
+module "vmss_extension_keyvault_extension" {
+  source     = "../modules/compute/virtual_machine_scale_set_extensions"
+  # version = "5.5.1"
+
+  #source = "git::https://github.com/VolkerWessels/terraform-azurerm-caf.git//modules/compute/virtual_machine_scale_set_extensions?ref=vw-combined"
+
+  depends_on = [module.example]
+
+  for_each = {
+    for key, value in try(var.virtual_machine_scale_sets, {}) : key => value
+    if try(value.virtual_machine_scale_set_extensions.microsoft_azure_keyvault, null) != null
+  }
+
+  client_config                = module.example.client_config
+  virtual_machine_scale_set_id = module.example.virtual_machine_scale_sets[each.key].id
+  extension                    = each.value.virtual_machine_scale_set_extensions.microsoft_azure_keyvault
+  extension_name               = "microsoft_azure_keyvault"
+  managed_identities           = tomap({ (var.landingzone.key) = module.example.managed_identities })
+}
