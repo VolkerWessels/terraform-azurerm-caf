c102658c1cea8c3d90bacb9e98681707601b85a2
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-15 15:35:00 +0100

    Remove var.global_settings from debug.tf

--- modules/compute/virtual_machine_scale_set_extensions/debug.tf
+++ modules/compute/virtual_machine_scale_set_extensions/debug.tf
@@ -3,12 +3,11 @@ resource "null_resource" "debug" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    command = "echo $VARIABLE1 >> debug.json; echo $VARIABLE2 >> debug.json; echo $VARIABLE3 >> debug.json; echo $VARIABLE4 >> debug.json; cat debug.json"
+    command = "echo $VARIABLE1 >> debug.json; echo $VARIABLE2 >> debug.json; echo $VARIABLE3 >> debug.json; cat debug.json"
     environment = {
-      VARIABLE1 = jsonencode(var.global_settings)
-      VARIABLE2 = jsonencode(local.managed_local_identity)
-      VARIABLE3 = jsonencode(local.managed_remote_identity)
-      VARIABLE4 = jsonencode(var.managed_identities)
+      VARIABLE1 = jsonencode(local.managed_local_identity)
+      VARIABLE2 = jsonencode(local.managed_remote_identity)
+      VARIABLE3 = jsonencode(var.managed_identities)
     }
   }
 }
