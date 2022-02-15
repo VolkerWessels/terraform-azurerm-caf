665be7ca1d4b018257f611b1ab9db0ddf64a9745
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-15 09:24:55 +0100

    Add keyvault extension module example

--- examples/vmss_extensions.tf
+++ examples/vmss_extensions.tf
@@ -14,7 +14,6 @@ module "vmss_extension_microsoft_azure_domainjoin" {
   keyvaults                    = tomap({ (var.landingzone.key) = module.example.keyvaults })
 }
 
-
 module "vmss_extension_custom_scriptextension" {
   source     = "../modules/compute/virtual_machine_scale_set_extensions"
   depends_on = [module.example]
@@ -31,4 +30,20 @@ module "vmss_extension_custom_scriptextension" {
   managed_identities                = tomap({ (var.landingzone.key) = module.example.managed_identities })
   storage_accounts                  = tomap({ (var.landingzone.key) = module.example.storage_accounts })
   virtual_machine_scale_set_os_type = module.example.virtual_machine_scale_sets[each.key].os_type
+}
+
+module "vmss_extension_keyvault_extension" {
+  source     = "../modules/compute/virtual_machine_scale_set_extensions"
+  depends_on = [module.example]
+
+  for_each = {
+    for key, value in try(var.virtual_machine_scale_sets, {}) : key => value
+    if try(value.virtual_machine_scale_set_extensions.microsoft_azure_keyvault, null) != null
+  }
+
+  client_config                = module.example.client_config
+  virtual_machine_scale_set_id = module.example.virtual_machine_scale_sets[each.key].id
+  extension                    = each.value.virtual_machine_scale_set_extensions.microsoft_azure_keyvault
+  extension_name               = "microsoft_azure_keyvault"
+  managed_identities           = tomap({ (var.landingzone.key) = module.example.managed_identities })
 }
