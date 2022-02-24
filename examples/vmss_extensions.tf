c14a751a682ea189ecd7fe3877f162f2a4b18020
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-24 09:36:20 +0100

    Add vmss_extension_application_health_extension example

--- examples/vmss_extensions.tf
+++ examples/vmss_extensions.tf
@@ -33,7 +33,7 @@ module "vmss_extension_custom_scriptextension" {
 }
 
 module "vmss_extension_keyvault_extension" {
-  #source     = "../modules/compute/virtual_machine_scale_set_extensions"
+  # source  = "../modules/compute/virtual_machine_scale_set_extensions"
   # version = "5.5.1"
 
   source = "git::https://github.com/VolkerWessels/terraform-azurerm-caf.git//modules/compute/virtual_machine_scale_set_extensions?ref=vw-combined"
@@ -50,4 +50,24 @@ module "vmss_extension_keyvault_extension" {
   extension                    = each.value.virtual_machine_scale_set_extensions.microsoft_azure_keyvault
   extension_name               = "microsoft_azure_keyvault"
   managed_identities           = tomap({ (var.landingzone.key) = module.example.managed_identities })
+}
+
+module "vmss_extension_application_health_extension" {
+  # source  = "../modules/compute/virtual_machine_scale_set_extensions"
+  # version = "5.5.1"
+
+  source = "git::https://github.com/VolkerWessels/terraform-azurerm-caf.git//modules/compute/virtual_machine_scale_set_extensions?ref=vw-combined"
+
+  depends_on = [module.example]
+
+  for_each = {
+    for key, value in try(var.virtual_machine_scale_sets, {}) : key => value
+    if try(value.virtual_machine_scale_set_extensions.microsoft_azure_health_extension, null) != null
+  }
+
+  client_config                     = module.example.client_config
+  virtual_machine_scale_set_id      = module.example.virtual_machine_scale_sets[each.key].id
+  virtual_machine_scale_set_os_type = module.example.virtual_machine_scale_sets[each.key].os_type
+  extension                         = each.value.virtual_machine_scale_set_extensions.microsoft_azure_health_extension
+  extension_name                    = "microsoft_azure_health_extension"
 }
