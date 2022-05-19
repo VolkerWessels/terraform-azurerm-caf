dc54fb8dd72b200346f0abe1fbbb5eade2fdeb51
hyperfocus1337 <hyperfocus@hotmail.com>
2022-05-19 15:33:15 +0200

    Add new debug block

---
+++ vw-debug.tf
@@ -0,0 +1,12 @@
+resource "null_resource" "debug" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = "echo $VARIABLE1 >> debug.json; echo $VARIABLE2 >> debug.json; cat debug.json"
+    environment = {
+      VARIABLE1 = jsonencode(try(local.azuread.azuread_groups, ""))
+      VARIABLE2 = jsonencode(try(local.combined_objects_azuread_groups, ""))
+    }
+  }
+}
