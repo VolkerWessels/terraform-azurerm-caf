497ad119a6dff010e6703caf9bcda3b7fa541e12
dimitrifc <>
2022-06-23 15:19:04 +0200

    module automations

--- automations.tf
+++ automations.tf
@@ -25,7 +25,7 @@ module "automation_log_analytics_links" {
   settings            = each.value
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   workspace_id =    try(each.value.log_analytics_workspace_id, try(var.diagnostics.log_analytics[each.value.log_analytics_key].id, null))
-  read_access_id = try(each.value.automation_account_id, try(var.automations[each.value.automation_account_key].id, null))
+  read_access_id = try(each.value.automation_account_id, try(module.automations[each.value.automation_account_key].id, null))
 }
 
 output "automation_log_analytics_links" {
