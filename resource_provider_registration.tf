bb2226a90e3dd5f67f9949bf36b376efeb27b539
Marcel Heek <mheek@vwinfra.nl>
2022-07-18 11:37:26 +0200

    fix resource_provider_regsitration.tf

--- resource_provider_registration.tf
+++ resource_provider_registration.tf
@@ -10,3 +10,15 @@ resource "azurerm_resource_provider_registration" "features" {
     }
   }
 }
+resource "azurerm_resource_provider_registration" "features" {
+  for_each = var.resource_provider_registration
+  name     = each.value.name
+
+  dynamic "feature" {
+    for_each = try(each.value.features, {}) == "features" ? [each.value.features] : []
+    content {
+      name       = each.key
+      registered = try(each.value, false)
+    }
+  }
+}
