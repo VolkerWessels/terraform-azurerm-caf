3a56901119a32a44a729576c22c6224700c94204
Berend <bvwaalwijk@volkerwessels.com>
2022-09-27 13:33:48 +0200

    added virtual subnets to postgresql

--- postgresql_servers.tf
+++ postgresql_servers.tf
@@ -7,6 +7,7 @@ output "postgresql_servers" {
 module "postgresql_servers" {
   source     = "./modules/databases/postgresql_server"
   depends_on = [module.keyvault_access_policies, module.keyvault_access_policies_azuread_apps]
+  depends_on = [module.keyvault_access_policies, module.keyvault_access_policies_azuread_apps]
   for_each   = local.database.postgresql_servers
 
 
@@ -18,6 +19,12 @@ module "postgresql_servers" {
   base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
   keyvault_id         = can(each.value.administrator_login_password) ? null : local.combined_objects_keyvaults[try(each.value.keyvault.lz_key, local.client_config.landingzone_key)][try(each.value.keyvault_key, each.value.keyvault.key)].id
 
+  keyvault_id = coalesce(
+    try(each.value.administrator_login_password, null),
+    try(module.keyvaults[each.value.keyvault_key].id, null),
+    try(local.combined_objects_keyvaults[each.value.keyvault.lz_key][each.value.keyvault.key].id, null),
+    try(local.combined_objects_keyvaults[local.client_config.landingzone_key][each.value.keyvault.key].id, null)
+  )
   storage_accounts    = module.storage_accounts
   azuread_groups      = module.azuread_groups
   vnets               = local.combined_objects_networking
