b270d84096bf21351f8b3f70647e7683cc0daf54
hyperfocus1337 <hyperfocus@hotmail.com>
2022-10-05 10:21:19 +0200

    Place debug in different location with locals

--- debug.tf
+++ debug.tf
@@ -1,8 +1,9 @@
 resource "null_resource" "debug" {
   provisioner "local-exec" {
-    command = "echo $VARIABLE1 > debug.json ; echo $VARIABLE2 >> debug.json ; echo $VARIABLE3 >> debug.json"
+    command = "echo $VARIABLE1 > debug.txt ; echo $VARIABLE2 >> debug.txt ; echo $VARIABLE3 >> debug.txt"
     environment = {
       VARIABLE1 = jsonencode(local.combined_objects_networking)
+      VARIABLE2 = jsonencode(local.client_config)
       VARIABLE2 = jsonencode(local.client_config)
       VARIABLE3 = jsonencode(local.compute.virtual_machines)
     }
