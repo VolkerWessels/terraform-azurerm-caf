9b0b153f4e251c7d79b6abb2bf1b42bcc0f87076
Tobias Peters <tpeters@volkerwessels.com>
2022-10-18 10:31:37 +0200

    mysql flex diff met postgres flex commit

--- mysql_flexible_servers.tf
+++ mysql_flexible_servers.tf
@@ -16,11 +16,13 @@ module "mysql_flexible_server" {
   location            = can(local.global_settings.regions[each.value.region]) || can(each.value.region) ? try(local.global_settings.regions[each.value.region], each.value.region) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
 
 remote_objects = {
+remote_objects = {
     private_dns_zone_id = can(each.value.private_dns_zone.key) ? local.combined_objects_private_dns[try(each.value.private_dns_zone.lz_key, local.client_config.landingzone_key)][each.value.private_dns_zone.key].id : null
     diagnostics         = local.combined_diagnostics
     vnets               = local.combined_objects_networking
-    # virtual_subnets     = local.combined_objects_virtual_subnets
+    virtual_subnets     = local.combined_objects_virtual_subnets
     private_dns         = local.combined_objects_private_dns
+    private_endpoints   = try(each.value.private_endpoints, {})
   }
 
   subnet_id           = can(each.value.subnet_id) || can(each.value.vnet_key) == false ? try(each.value.subnet_id, null) : local.combined_objects_networking[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
