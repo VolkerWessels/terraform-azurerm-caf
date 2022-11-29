f8feb4d19b571d754c95b0b6b3025a7aca534360
Jelle van de Haterd <JvdHaterd@volkerwessels.com>
2022-11-29 12:37:33 +0100

    azurerm_frontdoor_custom_https_configuration fix

--- modules/networking/front_door/front_door.tf
+++ modules/networking/front_door/front_door.tf
@@ -128,7 +128,7 @@ resource "azurerm_frontdoor" "frontdoor" {
 resource "azurerm_frontdoor_custom_https_configuration" "frontdoor" {
   for_each = {
     for key,value in var.settings.frontend_endpoints: key => merge(value, {frontend_endpoint_id=azurerm_frontdoor.frontdoor.frontend_endpoints[value.name]})
-    if can(frontend_endpoint_id=azurerm_frontdoor.frontdoor.frontend_endpoints[value.name])
+    if can(azurerm_frontdoor.frontdoor.frontend_endpoints[value.name])
   }
 
   frontend_endpoint_id = each.value.frontend_endpoint_id
