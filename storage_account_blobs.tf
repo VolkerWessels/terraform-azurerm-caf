c96398834dc5d4b5873769f3409d41c7cff10d5d
Berend <bvwaalwijk@volkerwessels.com>
2023-03-08 15:02:12 +0100

    storage account ref naar for_deferred verwijderd

--- storage_account_blobs.tf
+++ storage_account_blobs.tf
@@ -1,14 +1,6 @@
 #
 # Storage account blobs can be created as a nested object or isolated to allow RBAC to be set before writing the blob
 #
-
-resource "time_sleep" "delay" {
-  depends_on = [azurerm_role_assignment.for_deferred]
-  for_each   = local.storage.storage_account_blobs
-
-  create_duration = "300s"
-}
-
 module "storage_account_blobs" {
   source     = "./modules/storage_account/blob"
   depends_on = [time_sleep.delay]
