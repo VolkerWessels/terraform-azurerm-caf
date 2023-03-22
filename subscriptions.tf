1fe4eb3aeaf846e5a5ae03c6e0dc52eb09a40b09
Marcel Heek <mheek@volkerwessels.com>
2023-03-22 14:50:11 +0100

    change merge order

--- subscriptions.tf
+++ subscriptions.tf
@@ -9,7 +9,7 @@ module "subscriptions" {
   settings         = each.value
   client_config    = local.client_config
   diagnostics      = local.combined_diagnostics
-  tags             = merge(lookup(each.value, "tags", {}), var.tags)
+  tags             = merge(var.tags, lookup(each.value, "tags", {}))
 }
 
 module "subscription_billing_role_assignments" {
