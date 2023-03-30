b3e806e1254cb492b653677a9d155e1c486a546e
Yonathan Bensoussan <ybensoussan@thefactory.nl>
2023-03-30 20:07:00 +0200

    dynamic content block for app_settings

--- modules/webapps/appservice/main.tf
+++ modules/webapps/appservice/main.tf
@@ -27,12 +27,13 @@ locals {
 
   arm_filename = "${path.module}/arm_site_config.json"
 
-  app_settings = merge(try(var.app_settings, null), try(local.dynamic_settings_to_process, null), var.application_insight == null ? null :
+  app_settings = null_if_empty(merge(try(var.app_settings, {}), try(local.dynamic_settings_to_process, {}), var.application_insight == null ? {} :
     {
       "APPINSIGHTS_INSTRUMENTATIONKEY"             = var.application_insight.instrumentation_key,
       "APPLICATIONINSIGHTS_CONNECTION_STRING"      = var.application_insight.connection_string,
       "ApplicationInsightsAgent_EXTENSION_VERSION" = "~2"
     }
+    )
   )
 
   backup_storage_account = can(var.settings.backup) ? var.storage_accounts[try(var.settings.backup.lz_key, var.client_config.landingzone_key)][var.settings.backup.storage_account_key] : null
