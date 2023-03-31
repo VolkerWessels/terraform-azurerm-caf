bd512622b3470da0f4a9a62198e2bbd77f2ad45c
Yonathan Bensoussan <ybensoussan@thefactory.nl>
2023-03-31 10:14:32 +0200

    Update local to check if any of the maps are set, if not, then set app_settings to "null"

--- modules/webapps/appservice/main.tf
+++ modules/webapps/appservice/main.tf
@@ -15,8 +15,18 @@ locals {
 
   arm_filename = "${path.module}/arm_site_config.json"
 
-  app_settings = merge(try(var.app_settings, {}), try(local.dynamic_settings_to_process, {}), var.application_insight == null ? {} :
-    {
+  app_settings = length(merge(
+    try(var.app_settings, {}),
+    try(local.dynamic_settings_to_process, {}),
+    var.application_insight == null ? {} : {
+      "APPINSIGHTS_INSTRUMENTATIONKEY"             = var.application_insight.instrumentation_key,
+      "APPLICATIONINSIGHTS_CONNECTION_STRING"      = var.application_insight.connection_string,
+      "ApplicationInsightsAgent_EXTENSION_VERSION" = "~2"
+    }
+  )) == 0 ? null : merge(
+    try(var.app_settings, {}),
+    try(local.dynamic_settings_to_process, {}),
+    var.application_insight == null ? {} : {
       "APPINSIGHTS_INSTRUMENTATIONKEY"             = var.application_insight.instrumentation_key,
       "APPLICATIONINSIGHTS_CONNECTION_STRING"      = var.application_insight.connection_string,
       "ApplicationInsightsAgent_EXTENSION_VERSION" = "~2"
