f45459d03124208b96f1ec47df08d77918863c9e
Mathieu van Wijngaarden <mvwijngaarden@volkerwessels.com>
2023-04-05 14:14:15 +0200

    Add variables for private_end

--- modules/databases/postgresql_flexible_server/variables.tf
+++ modules/databases/postgresql_flexible_server/variables.tf
@@ -26,3 +26,16 @@ variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
   type        = bool
 }
+
+variable "location" {
+  description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
+  type        = string
+}
+
+variable "subnet_id" {
+  default = {}
+}
+
+variable "keyvault_id" {
+  default = {}
+}
