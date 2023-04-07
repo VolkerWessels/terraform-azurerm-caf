33a8ac043e44acc9fd8ae5f8495d3e8d533ca80c
Yonathan Bensoussan <ybensoussan@thefactory.nl>
2023-04-07 12:18:32 +0200

    Changes base_tags type back to map, from bool

--- modules/webapps/appservice/variables.tf
+++ modules/webapps/appservice/variables.tf
@@ -53,7 +53,7 @@ variable "global_settings" {
 
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
-  type        = bool
+  type        = map(any)
 }
 
 variable "combined_objects" {
