2de8c05e1734c90429ae935b8a5c93b521cbcaf0
BerendvW <bvwaalwijk@volkerwessels.com>
2023-09-11 14:41:38 +0200

    Revert "tags in private endpoint aangepast"
    This reverts commit f9d03197b77e7cb034ca67667d8351180136d037.

--- modules/networking/private_endpoint/main.tf
+++ modules/networking/private_endpoint/main.tf
@@ -8,12 +8,11 @@ terraform {
 }
 
 locals {
-  tags = var.base_tags
-  # ? merge(
-    # var.global_settings.tags,
-    # var.tags,
-    # try(var.settings.tags, null)
-  # ) : try(var.settings.tags, null)
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    var.tags,
+    try(var.settings.tags, null)
+  ) : try(var.settings.tags, null)
 
   location = can(var.location) || can(var.settings.region) ? try(var.location, var.global_settings.regions[var.settings.region]) : var.resource_groups[try(var.settings.resource_group.lz_key, var.settings.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)].location
 
