94dcf4c0944abf99c69e63e2322c4676dbdbf241
Marcel Heek <mheek@volkerwessels.com>
2023-09-19 10:53:47 +0200

    comment base_tags

--- modules/messaging/servicebus/namespace/private_endpoints.tf
+++ modules/messaging/servicebus/namespace/private_endpoints.tf
@@ -3,7 +3,6 @@ module "private_endpoint" {
   for_each = try(var.settings.private_endpoints, {})
 
   #base_tags       = local.base_tags
-  base_tags       = local.tags
   client_config   = var.client_config
   global_settings = var.global_settings
   location        = local.location
