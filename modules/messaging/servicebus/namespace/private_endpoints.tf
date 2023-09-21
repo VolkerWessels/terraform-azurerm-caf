daa41c7595992196d97fa888bcad9ba65274e358
Yonathan Bensoussan <ybensoussan@thefactory.nl>
2023-09-21 11:19:26 +0200

    private endpoint module call incorrect resource_group parameter

--- modules/messaging/servicebus/namespace/private_endpoints.tf
+++ modules/messaging/servicebus/namespace/private_endpoints.tf
@@ -8,7 +8,7 @@ module "private_endpoint" {
   location            = local.location
   name                = each.value.name
   private_dns         = can(each.value.private_dns) ? var.remote_objects.private_dns : {}
-  resource_group_name = local.resource_group.name
+  resource_group_name = local.resource_group_name
   resource_id         = azurerm_servicebus_namespace.namespace.id
   settings            = each.value
   subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : var.remote_objects.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
