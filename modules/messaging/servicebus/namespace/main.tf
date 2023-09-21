c26b38b64952d7ba83de866119b336df4cc96335
Yonathan Bensoussan <ybensoussan@thefactory.nl>
2023-09-21 11:02:28 +0200

    base_tags is a local not a variable

--- modules/messaging/servicebus/namespace/main.tf
+++ modules/messaging/servicebus/namespace/main.tf
@@ -11,7 +11,7 @@ locals {
   location            = can(var.settings.location) ? var.settings.location : var.remote_objects.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.resource_group.key].location
   resource_group_name = can(var.settings.resource_group_name) || can(var.settings.resource_group.name) ? try(var.settings.resource_group_name, var.settings.resource_group.name) : var.remote_objects.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.resource_group.key].name
   base_tags           = can(var.settings.base_tags) ?  var.settings.base_tags : try(var.global_settings.inherit_tags, false)
-  tags = var.base_tags ? merge(
+  tags = local.base_tags ? merge(
     var.global_settings.tags,
     var.tags,
     try(var.settings.tags, null)
