40647896b8b4dafedcd05e3e288d7c91f0283ca0
Marcel Heek <mheek@volkerwessels.com>
2023-09-28 17:47:20 +0200

    try add local for tags

--- modules/logic_app/standard/locals.dynamic_app_settings.tf
+++ modules/logic_app/standard/locals.dynamic_app_settings.tf
@@ -24,4 +24,8 @@ locals {
       ]
     ) : setting.key => setting.value
   }
+  module_tag = {
+    "module" = basename(abspath(path.module))
+  }
+  tags = merge(var.base_tags, local.module_tag, try(var.tags, null))
 }
