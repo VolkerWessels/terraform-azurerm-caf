b1146505e798167c887c8e106b93f74593cf2713
Marcel Heek <mheek@volkerwessels.com>
2023-10-04 17:19:48 +0200

    add resource_group

--- virtual_machines_extensions.tf
+++ virtual_machines_extensions.tf
@@ -114,6 +114,7 @@ module "vm_extension_generic" {
   extension_name          = "generic_extension"
   base_tags               = local.global_settings.inherit_tags
   global_settings         = local.global_settings
+  resource_group          = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
 }
 
 module "keyvault_for_windows" {
