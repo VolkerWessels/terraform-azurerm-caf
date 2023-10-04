820a9018006a01130e11f6875dd4d13c8c1003a4
Marcel Heek <mheek@volkerwessels.com>
2023-10-04 15:50:57 +0200

    fix

--- logic_app.tf
+++ logic_app.tf
@@ -160,6 +160,7 @@ module "logic_app_standard" {
   base_tags           = local.global_settings.inherit_tags
   resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
+  location            = try(local.global_settings.regions[each.value.region], null)
 }
 
 output "logic_app_standard" {
